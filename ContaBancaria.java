7c7277ba6e07a5074ffe9f1281b9bdc777e5c04f
DiogodeCristo <estudosadsdiogoav@gmail.com>
2025-06-25 21:23:09 -0300

    Update ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -26,3 +26,32 @@ public abstract class ContaBancaria {
     public double getSaldo() { return saldo; }
     public String getTitular() { return titular; }
 }
+
+public class ContaCorrente extends ContaBancaria {
+    private double taxaManutencao;
+
+    public ContaCorrente(String numeroConta, String titular, double taxaManutencao) {
+        super(numeroConta, titular);
+        this.taxaManutencao = taxaManutencao;
+    }
+
+    @Override
+    public boolean sacar(double valor) {
+        double valorComTaxa = valor + taxaManutencao;
+        return super.sacar(valorComTaxa);
+    }
+}
+
+public class ContaPoupanca extends ContaBancaria {
+    private double taxaRendimento;
+
+    public ContaPoupanca(String numeroConta, String titular, double taxaRendimento) {
+        super(numeroConta, titular);
+        this.taxaRendimento = taxaRendimento;
+    }
+
+    public void aplicarRendimento() {
+        double rendimento = getSaldo() * taxaRendimento;
+        depositar(rendimento);
+    }
+}
